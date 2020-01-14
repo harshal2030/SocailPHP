16134103207ac40363010cce5e7c40701a8a1c1f
Harshal <harshal2030@outlook.com>
2020-01-14 23:06:41 +0530

    need to fix little bug

---
+++ includes/classes/ProfileClass.php
@@ -0,0 +1,28 @@
+<?php
+
+class ProfileDataHandler {
+    private $con;
+
+    public function __construct($con) {
+        $this->con = $con;
+    }
+
+    public function validateEmail($em) {
+        if (!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function fetchProfileData($em) {
+        $query = $this->con->prepare("SELECT name, admission, profilepic FROM users WHERE email=:em");
+        $query->bindParam(":em", $em);
+        $query->execute();
+
+        $data = $query->fetch(PDO::FETCH_ASSOC);
+        return json_encode($data);
+    }
+}
+
+?>
---
+++ profile.php
@@ -0,0 +1,34 @@
+<?php
+require_once("includes/config.php");
+require_once("includes/classes/FormSanitizer.php");
+require_once("includes/classes/ProfileClass.php");
+
+if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) {
+
+    $username = FormSanitizer::sanitizeString($_SERVER['PHP_AUTH_USER']);
+    $pw = FormSanitizer::sanitizePassword($_SERVER['PHP_AUTH_PW']);
+    $pw = hash("sha512", $salt1.$pw.$salt2);
+
+    $query = $con->prepare("SELECT * FROM authenticate WHERE username=:un && password=:pw");
+    $query->bindParam(":un", $username);
+    $query->bindParam(":pw", $pw);
+
+    $query->execute();
+    if ($query->rowCount() == 1) {
+        $json = file_get_contents('php://input');
+
+        $controller = new ProfileDataHandler($con);
+        $data = json_decode($json, true);
+        $email = $data['email'];
+
+        if ($controller->validateEmail($email)) {
+            echo $controller->fetchProfileData($em);
+        }
+    }
+}
+else {
+    header("WWW-Authenticate: Basic realm='Restricted Section'");
+    header("HTTP/1.0 401 Unauthorized");
+    die("Please enter your username and password");
+}
+?>
