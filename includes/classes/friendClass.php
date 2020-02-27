44af5587de14569d371f9dc306d747336e849921
harshal <harshal2030@outlook.com>
2020-02-28 01:06:19 +0530

    last step to add friend

--- includes/classes/friendClass.php
+++ includes/classes/friendClass.php
@@ -8,43 +8,16 @@ class FriendHandler {
         $this->con = $con;
     }
 
-    public function addFriend($userEmail, $user, $friendEmail, $friend) {
-        $query = $this->con->prepare("INSERT INTO friends(userEmail, user, followingEmail, following) 
-                                    VALUES(:uemail, :un, :femail, :fw)");
-        $query->bindParam(":uemail", $userEmail);
-        $query->bindParam(":un", $user);
-        $query->bindParam(":femail", $friendEmail);
-        $query->bindParam(":fw", $friend);
+    public function addFriend($username, $name, $folowingUserName, $following) {
+        $query = $this->con->prepare("INSERT INTO friends(username, user, followingUserName, following)
+                                    VALUES(:un, :unm, :FUN, :fw)");
+        
+        $query->bindParam(':un', $username);
+        $query->bindParam(':unm', $name);
+        $query->bindParam(':FUN', $folowingUserName);
+        $query->bindParam(':fw', $following);
 
         return $query->execute();
-    }
-
-    public function fetchFollowing($userEmail) {
-        $query = $this->con->prepare("SELECT following FROM friends WHERE userEmail=:uemail");
-        $query->bindParam(":uemail", $userEmail);
-        $query->execute();
-
-        $following = [];
-
-        while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-            array_push($following, $row['following']);
-        }
-
-        return json_encode($following);
-    }
-
-    public function fetchFollower($userEmail) {
-        $query = $this->con->prepare("SELECT user FROM friends WHERE followingEmail=:uemail");
-        $query->bindParam(":uemail", $userEmail);
-        $query->execute();
-
-        $follower = [];
-
-        while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-            array_push($follower, $row['user']);
-        }
-
-        return json_encode($follower);
     }
 
 
